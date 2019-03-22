fba38cb0903777476c96b66af964727d5cb3457b
Vukasin Stancevic <stihilus@gmail.com>
2019-03-22 14:19:54 +0100

    randoooom

--- sketch.js
+++ sketch.js
@@ -3,18 +3,30 @@
 function setup() {
 	var w = windowWidth;
 	var h = windowHeight;
-  	var canvas = createCanvas(w * 0.792, h - 500);
+  	var canvas = createCanvas(w * 0.792, h - 400);
   	// Move the canvas so it's inside our <div id="sketch-holder">.
   	canvas.parent('sketch-holder');
+  	frameRate(1); // Attempt to refresh at starting FPS
 
   	background(200);
 }
 
 
 function draw() {
-  stroke(155);
-  strokeWeight(4); // Thicker
-  if (mouseIsPressed === true) {
-    line(mouseX, mouseY, pmouseX, pmouseY);
-  }
+	background(200);
+	var c = mouseX * 0.22;
+	noStroke();
+	fill(c);
+
+	// Draw 
+	var cW = windowWidth * 0.35;
+	var cH = windowHeight * 0.27;
+
+	circle(cW, cH, random(5,50));
+	circle(cW + random(100), cH, random(5,100));
+	rect(cW + random(100), cH - random(100), random(200), random(100));
+	rect(cW + random(100), cH - random(100), random(100), random(100));
+	rect(cW + random(200), cH - random(300), random(500), random(100));
+	rect(cW + random(200), cH - random(400), random(100), random(100));
+
 }
