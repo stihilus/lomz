79eabf3d376559c3db185371a7e73c2ebe380c86
Vukasin Stancevic <stihilus@gmail.com>
2019-03-26 12:21:03 +0100

    new p5js sketch on homepage

---
+++ reset.js
@@ -0,0 +1,5 @@
+function reset() {
+resetMatrix(); // reset canvas
+imageMode(CENTER);
+translate(width / 2, height / 2);
+}
--- sketch.js
+++ sketch.js
@@ -3,30 +3,74 @@
 function setup() {
 	var w = windowWidth;
 	var h = windowHeight;
-  	var canvas = createCanvas(w * 0.792, h - 400);
+  	var canvas = createCanvas(w * 0.79, h - 400);
   	// Move the canvas so it's inside our <div id="sketch-holder">.
   	canvas.parent('sketch-holder');
   	frameRate(1); // Attempt to refresh at starting FPS
 
   	background(200);
+
+  	// SETOVANJE
+  	translate(width / 2, height / 2); // pozicioniranje slike na sredinu canvasa
+  	imageMode(CENTER); // imageMode odredjuje da ce se rotiranje slike vrsiti iz njenog centra
+  	background(230);
+
+	// OKVIR GRAFIKE - PARSPARTU
+	resetMatrix(); //  resetovanje svih podesavanja za canvas
+	translate(width / 2, height / 2);
+	imageMode(CENTER);
+
+	//first graphic
+	 // Layer 01 - povrsine
+  	reset();
+  	var images_skener = [img02, img03, img04, img05, img21];
+  	var slika_skener = random(images_skener); // select random image from images array
+  	rotate(random(380));
+  	translate(random(-500, 500), random(-500, 500));
+  	image(slika_skener, 0, 0); // stampanje slike
 }
 
 
 function draw() {
-	background(200);
-	var c = mouseX * 0.22;
-	noStroke();
-	fill(c);
+}
 
-	// Draw 
-	var cW = windowWidth * 0.35;
-	var cH = windowHeight * 0.27;
+function mouseClicked() {
+	// Set Background
+	background(220);
 
-	circle(cW, cH, random(5,50));
-	circle(cW + random(100), cH, random(5,100));
-	rect(cW + random(100), cH - random(100), random(200), random(100));
-	rect(cW + random(100), cH - random(100), random(100), random(100));
-	rect(cW + random(200), cH - random(300), random(500), random(100));
-	rect(cW + random(200), cH - random(400), random(100), random(100));
+	// Layer 01 - povrsine
+	reset();
+	var images_skener = [img02, img03, img04, img05, img21];
+	var slika_skener = random(images_skener); // select random image from images array
+	rotate(random(380));
+	translate(random(-500, 500), random(-500, 500));
+	image(slika_skener, 0, 0); // stampanje slike
 
+	// Layer 02 - providne sare
+	reset();
+	var images_skener = [img06, img07, img08, img09, img10];
+	var slika_skener = random(images_skener); // select random image from images array
+	//translate(random(-500, 500), random(-500, 500));
+	image(slika_skener, 0, 0); // stampanje slike
+
+	// Layer 02 - elektronika
+	reset();
+	var images_skener = [img11, img12, img13, img14, img15, img16, img17, img18];
+	var slika_skener = random(images_skener); // select random image from images array
+	translate(random(-300, 300), random(-300, 300));
+	image(slika_skener, 0, 0); // stampanje slike
+
+	// Layer 02 - elektronika
+	reset();
+	var images_skener = [img11, img12, img13, img14, img15, img16, img17, img18];
+	var slika_skener = random(images_skener); // select random image from images array
+	translate(random(-300, 300), random(-300, 300));
+	image(slika_skener, 0, 0); // stampanje slike
+
+	// Layer 02 - elektronika
+	reset();
+	var images_skener = [img11, img12, img13, img14, img15, img16, img17, img18];
+	var slika_skener = random(images_skener); // select random image from images array
+	translate(random(-300, 300), random(-300, 300));
+	image(slika_skener, 0, 0); // stampanje slike
 }
